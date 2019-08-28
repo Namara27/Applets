0b0bb5013119ab5b428564e116d983d11d1591c4
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-28 19:23:57 +0200

    vierde commit ofzo

--- src/Opdracht2_1/Naam.java
+++ src/Opdracht2_1/Naam.java
@@ -1,4 +1,21 @@
 package Opdracht2_1;
 
-public class Naam {
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+public class Naam extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.blue);
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Namara", 50, 60 );
+    }
+
+
 }
+
