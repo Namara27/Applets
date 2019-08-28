3a123c28daf70d7f4aabde402e52e974c9d968d3
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-08-28 19:32:50 +0200

    tweede commit = opdracht

--- src/Opdracht2_2/Naam2.java
+++ src/Opdracht2_2/Naam2.java
@@ -1,4 +1,19 @@
 package Opdracht2_2;
 
-public class Naam2 {
+import java.awt.*;
+import java.applet.*;
+
+public class Naam2 extends Applet {
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.white);
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Namara", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Kerkenaar", 50, 70);
+    }
+
 }
